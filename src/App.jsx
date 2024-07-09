331d9da78d649c2dc9170cc0d526f11ab76219e5
Saroj <sarojregmi.official@gmail.com>
2024-07-09 22:50:26 +0545

    Moved to vite

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+import Alert from "./components/Alert";
+import { useState } from "react";
+import About from "./components/About";
+
+function App() {
+    const [alert, setAlert] = useState(null);
+    const showalert = (message, type) => {
+        setAlert({
+            msg: message,
+        });
+        setTimeout(() => {
+            setAlert(null);
+        }, 1500);
+    };
+
+    const [changecolor, setchangecolor] = useState("darkmode");
+
+    const [mode, setMode] = useState("light");
+    const click = () => {
+        if (mode === "light") {
+            if (changecolor === "darkmode") {
+                setMode("dark");
+                document.body.style.backgroundColor = "black";
+                showalert("darkmode on ", "success");
+                setchangecolor("greenmode");
+            }
+        } else if (mode === "dark") {
+            setMode("green");
+            document.body.style.backgroundColor = "green";
+            showalert("greenmode on", "success");
+            setchangecolor("redmode");
+        } else if (mode === "green") {
+            setMode("red");
+            document.body.style.backgroundColor = "red";
+            showalert("redmode on", "success");
+            setchangecolor("Orangemode");
+        } else if (mode === "red") {
+            setMode("orange");
+            document.body.style.backgroundColor = "orange";
+            showalert("orangemode on", "success");
+            setchangecolor("Lightmode");
+        } else {
+            setMode("light");
+            document.body.style.backgroundColor = "white";
+            showalert("whitemode on", "success");
+            setchangecolor("darkmode");
+        }
+    };
+    return (
+        <>
+            <Navbar
+                title="Sagar"
+                link="About"
+                homepage="HOME"
+                mood={mode}
+                click={click}
+                hi={changecolor}
+            />
+            <Alert sap={alert} />
+            <Form hi={showalert} head="enter your thoughts" mood={mode} />
+            <About />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+
+export default function About() {
+
+
+    const [count, setCount] = useState(
+        {
+            color: 'black',
+            backgroundColor: 'white'
+
+        }
+    )
+    const [chbar, setChbar] = useState('dark mode')
+
+    const mode = () => {
+        if (count.color === 'white') {
+            setCount({
+                color: 'black',
+                backgroundColor: 'white',
+                border: '1px solid black'
+
+
+            })
+            setChbar('dark mode')
+        }
+        else {
+            setCount({
+                color: 'white',
+                backgroundColor: 'black',
+                border: '1px solid white'
+
+            })
+            setChbar('light mode')
+        }
+
+    }
+
+
+
+    return (
+        <div className='mainbox' style={count}>
+            <h2 className="hello my-3">About Us</h2>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button" style={count} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Accordion Item #1
+                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                        <div className="accordion-body" style={count}>
+                            <p>hello</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" style={count} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Accordion Item #2
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body" style={count}>
+                            <p>hiii</p>            </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" style={count} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Accordion Item #3
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body" style={count}>
+                            <p>byeeeeee</p>            </div>
+                    </div>
+                </div>
+            </div>
+            <div className="cont my-2">
+                <button type="button" className="btn btn-primary" onClick={mode} >{chbar}</button>
+            </div>
+        </div>
+    )
+}
